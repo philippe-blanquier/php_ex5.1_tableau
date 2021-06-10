8fb7332db96b5782c5bc306028f8bceedb1c68f6
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-10 10:53:34 +0200

    creation

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 4.8 fonction</title>
+    </head>
+    <body >
+        <p>
+            <?php
+            $monthArray= array('janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre');
+            var_dump($monthArray);
+            ?>
+       </p>
+    </body>
+</html>
